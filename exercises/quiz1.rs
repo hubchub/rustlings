a1452f01bb0d55668939febc98ace07544e6e17e
Connor Hubbard <connorhubbard@utexas.edu>
2023-07-15 15:28:14 -0500

    genesis

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x: i32 ) -> i32 {
+    let mut multiplier = 2;
+    if x > 40 { multiplier = 1 }
+    multiplier * x
+}
 
 // Don't modify this function!
 #[test]
